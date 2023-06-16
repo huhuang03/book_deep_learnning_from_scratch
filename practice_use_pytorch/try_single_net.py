791aafddb8ef8e559201e62c123dc57873e68a56
胡文凡 <wenfan.hu@infaith.com.cn>
2023-06-16 18:02:32 +0800

    up

--- practice_use_pytorch/try_single_net.py
+++ practice_use_pytorch/try_single_net.py
@@ -10,8 +10,8 @@ img0 = train_dataset.data[0].numpy()
 height, width = img0.shape
 img_size = height * width
 
-# model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10), torch.nn.Softmax())
-model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10))
+model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10), torch.nn.Sigmoid())
+# model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10))
 loss_fn = torch.nn.CrossEntropyLoss()
 
 
@@ -19,18 +19,24 @@ batch_size = 64
 dataloader = DataLoader(train_dataset, batch_size)
 optimizer = torch.optim.SGD(model.parameters(), 0.005)
 
-model.train()
+test_data = train_dataset.test_data
+test_label = train_dataset.test_labels
+
 for batch_idx, (batch_data, batch_label) in enumerate(dataloader):
+    model.train()
     optimizer.zero_grad()
     y = model(batch_data)
     loss = loss_fn(y, batch_label)
     loss.backward()
     optimizer.step()
 
-    _, predicted = torch.max(y, 1)
-    correct = (predicted == batch_label).sum().item()
-    accuracy = correct / batch_label.size(0)
-    print(f"in train data: Epoch [Batch [{batch_idx + 1}/{len(dataloader)}], Accuracy: {accuracy:.4f}")
+    model.eval()
+    # with torch.no_grad():
+    #     test_y = model(test_data)
+    #     _, predicted = torch.max(test_y, 1)
+    #     correct = (predicted == test_label).sum().item()
+    #     accuracy = correct / test_label.size(0)
+    #     print(f"in train data: Epoch [Batch [{batch_idx + 1}/{len(dataloader)}], Accuracy: {accuracy:.4f}")
 
 # 网络
 # Line -> Relu -> Line -> Softmax
