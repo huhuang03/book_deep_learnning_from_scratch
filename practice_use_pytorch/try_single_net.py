7dc4b24d16b7b0ea9a4bcc13153c172a8cea5f6f
胡文凡 <wenfan.hu@infaith.com.cn>
2023-07-03 09:45:24 +0800

    up

--- practice_use_pytorch/try_single_net.py
+++ practice_use_pytorch/try_single_net.py
@@ -4,16 +4,13 @@ from torch.utils.data import DataLoader
 
 # load FashionMNIST data.
 train_dataset = datasets.MNIST(root='runtime', transform=transforms.ToTensor(), download=True, train=True)
-imgs = train_dataset.data.numpy()
-img0 = train_dataset.data[0].numpy()
+test_dataset = datasets.MNIST(root='runtime', transform=transforms.ToTensor(), download=True, train=False)
+test_loader = DataLoader(test_dataset, batch_size=test_dataset.data.shape[0])
 
-height, width = img0.shape
-img_size = height * width
+img_size = train_dataset.data.shape[1] * train_dataset.data.shape[2]
 
-model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10), torch.nn.Sigmoid())
-# model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10))
+model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(img_size, 10), torch.nn.Softmax())
 loss_fn = torch.nn.CrossEntropyLoss()
-
 
 batch_size = 64
 dataloader = DataLoader(train_dataset, batch_size)
@@ -22,31 +19,35 @@ optimizer = torch.optim.SGD(model.parameters(), 0.005)
 test_data = train_dataset.test_data
 test_label = train_dataset.test_labels
 
-for batch_idx, (batch_data, batch_label) in enumerate(dataloader):
-    model.train()
-    optimizer.zero_grad()
-    y = model(batch_data)
-    loss = loss_fn(y, batch_label)
-    loss.backward()
-    optimizer.step()
 
+def calc_accuracy():
     model.eval()
-    # with torch.no_grad():
-    #     test_y = model(test_data)
-    #     _, predicted = torch.max(test_y, 1)
-    #     correct = (predicted == test_label).sum().item()
-    #     accuracy = correct / test_label.size(0)
-    #     print(f"in train data: Epoch [Batch [{batch_idx + 1}/{len(dataloader)}], Accuracy: {accuracy:.4f}")
+    with torch.no_grad():
+        # label is [10000]
+        for images, labels in test_loader:
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            correct = (predicted == labels).sum().item()
+            print(f'correct: {correct}')
+            accuracy = correct / labels.size(0)
+            return accuracy
 
-# 网络
-# Line -> Relu -> Line -> Softmax
-# lost: CrossEntropyCross
 
-# class TwoLayerNet:
-#     def __init__(self):
-#         self.flatten = torch.nn.Flatten()
-#         self.nets = torch.nn.Sequential([
-#             torch.nn.Linear(img_size, 10)
-#         ])
-#         # 首先全联接
-#         pass
+def train():
+    epoch = 1
+    for epoch in range(0, 100):
+        for batch_idx, (batch_data, batch_label) in enumerate(dataloader):
+            model.train()
+            optimizer.zero_grad()
+            y = model(batch_data)
+            loss = loss_fn(y, batch_label)
+            loss.backward()
+            optimizer.step()
+            # how to do this?
+            # print(f"in train data: Epoch {epoch} [Batch [{batch_idx + 1}/{len(dataloader)}], lose: {loss.item()}")
+        accuracy = calc_accuracy()
+        print(f"Epoch {epoch}, accuracy: {accuracy}")
+
+
+if __name__ == '__main__':
+    train()
